b42bb5f6ec0d32ffa768f9ee495ecb026b06106c
Rei <reisegev@yahoo.com>
2020-12-28 18:35:21 +0200

    upload gam,e

---
+++ Assets/MapManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public float xMin, xMax;
+    public float yMin, yMax;
+     public   int spaceBetweenDots;
+
+    public static MapManager _instance;
+    [SerializeField] GameObject LineObject;
+    LineRenderer LineComponent;
+    EdgeCollider2D mapCollider;
+    Vector2[] linePoints;
+    RaycastHit2D[] _hitinfo;
+    void Start()
+    {
+        _instance = this;
+        mapCollider = LineObject.GetComponent<EdgeCollider2D>();
+        LineComponent = LineObject.GetComponent<LineRenderer>();
+        CreateMap();
+    }
+
+
+    void CreateMap()
+    {
+        LineComponent.positionCount = Mathf.RoundToInt((Mathf.Abs(xMin) + Mathf.Abs(xMax)) / 2) + 1;
+        LineComponent.SetPosition(0, new Vector2(xMin, UnityEngine.Random.Range(yMin, yMax)));
+        linePoints= new Vector2[LineComponent.positionCount];
+        linePoints[0] = LineComponent.GetPosition(0);
+        for (int i = 1; i < LineComponent.positionCount; i++)
+        {
+            LineComponent.SetPosition(i, GetRandomPoint(LineComponent.GetPosition(i - 1).x));
+            linePoints[i]= LineComponent.GetPosition(i);
+            if (LineComponent.GetPosition(i).x >= xMax)
+            {
+                break;
+            }
+        
+      }
+            mapCollider.points = linePoints;
+  
+
+    }
+    Vector2 GetRandomPoint(float previousXPoint)
+    {
+        int newXPoint = Mathf.RoundToInt(previousXPoint) + spaceBetweenDots;
+
+        int yPoint = Mathf.RoundToInt(UnityEngine.Random.Range(yMin, yMax));
+
+
+        return new Vector2Int(newXPoint, yPoint);
+    }
+   public float GetAnglesOfTwoPoints(Vector2 point) {
+        float x = point.x;
+      
+        for (int i = 0; i < linePoints.Length -1; i++)
+        {
+            if (linePoints[i].x < x && x < linePoints[i+1].x)
+            {
+               
+                float f = (Mathf.Atan2(linePoints[i + 1].y- linePoints[i].y, linePoints[i + 1].x - linePoints[i].x) * Mathf.Rad2Deg) ;
+                if (f < 1 && f > -1)
+                {
+                    f= 0;
+                }
+
+                return  f;
+            }
+        }
+
+        return 0;
+    }
+
+
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    public float speed , torqueSpeed;
+    bool angleCheck, velocityCheck;
+    [SerializeField] Transform winText, lostText;
+
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Controllers();
+        CheckLandingAngleIsOk();
+    
+    }
+
+
+    void Controllers() {
+
+        float Force = Input.GetAxis("Vertical");
+        float torque = -Input.GetAxis("Horizontal");
+
+
+        _rb.AddTorque((torque / 2) * torqueSpeed, ForceMode2D.Force);
+        _rb.AddRelativeForce(new Vector2(0, Force * speed), ForceMode2D.Force);
+
+        // Debug.Log(_rb.velocity.magnitude);
+        velocityCheck = CheckVelocity();
+        Debug.Log("velocityCheck : " + velocityCheck);  
+        Debug.Log("_rb.velocity.magnitude : " + _rb.velocity.magnitude);  
+    }
+
+    bool CheckLandingAngleIsOk()
+    {
+        Vector3 direction = new Vector3(Mathf.Cos((transform.eulerAngles.z +90f)*Mathf.Deg2Rad) , Mathf.Sin((-transform.eulerAngles.z + 90f) * Mathf.Deg2Rad), 0);
+  
+        Debug.DrawLine(transform.position, transform.position - direction.normalized);
+
+      
+        float f= (Mathf.Atan2(direction.normalized.y, direction.normalized.x) * Mathf.Rad2Deg);
+        float x = f - MapManager._instance.GetAnglesOfTwoPoints(transform.position);
+
+        angleCheck = (x > 80f && x < 100f);
+        Debug.Log(angleCheck);
+
+        return angleCheck;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool didntCrash = true;
+        Debug.Log("Landed");
+        if (angleCheck && velocityCheck && didntCrash)
+        {
+            winText.gameObject.SetActive(true);
+            _rb.simulated = false;
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        else
+        {
+            didntCrash = false;
+            lostText.gameObject.SetActive(true);
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    } 
+
+    private bool CheckVelocity()
+    {
+        return _rb.velocity.magnitude< 2f;
+     
+    }
+}
